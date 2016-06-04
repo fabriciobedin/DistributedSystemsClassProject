9868550ec7ad1afb8c006cbdb76ee1cf67b3d63b
Fabricio Bedin <fabricioobedin@gmail.com>
2016-06-04 18:49:40 -0300

    funcionando (falta terminar)

--- src/java/br/upf/sd/servertcp/ServerTCP.java
+++ src/java/br/upf/sd/servertcp/ServerTCP.java
@@ -5,10 +5,6 @@
  */
 package br.upf.sd.servertcp;
 
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,27 +13,25 @@ import java.net.Socket;
  * @author fabricio
  */
 public class ServerTCP{
-    
 
     public static void main(String[] args) {
         int porta = 2006;
+        boolean conectado = true;
         try {
             ServerSocket servidor = new ServerSocket(porta);
             System.out.println("servidor ouvindo a porta 2006");
-            while (true) {
+            
+            while (conectado) {
                 Socket cliente = servidor.accept();
                 System.out.println("cliente conectado: " + cliente.getInetAddress().getHostAddress());
                 
-                new ThreadCliente(cliente).start();
-
+                ThreadServer clienteConectado = new ThreadServer(cliente);
+                clienteConectado.start();
             }
 
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
-
     }
-    
-   //http://www.devmedia.com.br/java-sockets-criando-comunicacoes-em-java/9465
-    
+
 }
