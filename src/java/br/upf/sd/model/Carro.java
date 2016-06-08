80b66d4c4fee9b20a0f66f42128936f1a97f3763
Victor <Victor@DESKTOP-2N3HQ9I>
2016-06-07 23:50:54 -0300

    Finalizado Método de Consulta

--- src/java/br/upf/sd/model/Carro.java
+++ src/java/br/upf/sd/model/Carro.java
@@ -21,7 +21,7 @@ public class Carro {
     private String complemento;
 
     public Carro() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public int getCodigo() {
--- src/java/br/upf/sd/serversoap/ServerSoap.java
+++ src/java/br/upf/sd/serversoap/ServerSoap.java
@@ -5,9 +5,19 @@
  */
 package br.upf.sd.serversoap;
 
-import javax.jws.WebService;
+import br.upf.sd.model.Carro;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.soap.SOAPFault;
+import org.json.JSONArray;
+
 
 /**
  *
@@ -17,10 +27,69 @@ import javax.jws.WebParam;
 public class ServerSoap {
 
     /**
-     * This is a sample web service operation
+     * Web service operation
+     *
+     * @param codigo
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName = "Consulta")
+    public Carro Consulta(@WebParam(name = "codigo") int codigo) {
+        Carro carro = new Carro();
+        
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            //TODO implementar SOAPFault
+        }
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://ec2-52-67-44-247.sa-east-1.compute.amazonaws.com:5432/trabalhosd",
+                    "postgres",
+                    ""
+            );
+
+        } catch (SQLException e) {
+            //TODO implementar SOAPFault
+        }
+        
+
+ 
+        if (conn != null) {
+
+            try {
+
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM carro WHERE codigo = 1");
+                while (rs.next()) {
+                   carro = populaCarro(rs);
+                }
+                rs.close();
+                st.close();
+
+            } catch (SQLException e) {
+                //TODO implementar SOAPFault
+            }
+
+        } else {
+            //TODO implementar SOAPFault
+        }
+        return carro;
     }
+
+    private static Carro populaCarro(ResultSet rs) throws SQLException {
+        Carro carro = new Carro();
+
+        carro.setCodigo(rs.getInt("codigo"));
+        carro.setMarca(rs.getString("marca"));
+        carro.setModelo(rs.getString("modelo"));
+        carro.setAno(rs.getInt("ano"));
+        carro.setPotencia(rs.getFloat("potencia"));
+        carro.setCarga(rs.getFloat("carga"));
+        carro.setComplemento(rs.getString("complemento"));
+
+        return carro;
+    }
+
 }
