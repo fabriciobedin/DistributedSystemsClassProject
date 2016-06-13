43ec318b80a5baca88c6fa1caeaa11a72b9504b8
Fabricio Bedin <fabricioobedin@gmail.com>
2016-06-12 23:28:43 -0300

    adição do método fechar conexão

--- src/java/br/upf/sd/factory/ConexaoBanco.java
+++ src/java/br/upf/sd/factory/ConexaoBanco.java
@@ -10,7 +10,6 @@ package br.upf.sd.factory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
